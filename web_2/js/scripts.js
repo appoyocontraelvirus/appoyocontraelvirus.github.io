a2997c1d4034e1c67b8de31bd32cd30bd5900d49
ManuConcepBrito <33829944+ManuConcepBrito@users.noreply.github.com>
2020-04-04 08:20:25 +0100

    Update scripts.js

--- web_2/js/scripts.js
+++ web_2/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
     (function($) {
   "use strict"; // Start of use strict
 
